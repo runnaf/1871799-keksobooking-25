04d8746143025092e74e178c3b3247b0a861dfec
Irina Lukyanova <runnaf@mail.ru>
2022-02-19 16:33:55 +0300

    Добавила в JS функции вывода случайного целого числа и с плавающей точкой

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
-function getSum (a, b) {
-  const description = 'Сумма двух чисел равна';
-  const sum = a + b;
-  return `${description  } ${  sum}`;
+function rangeNumbers (fromNumber, toNumber) {
+  if (fromNumber >= 0 && toNumber > fromNumber) {
+    if (toNumber === parseInt(toNumber, 10)) {
+      return Math.floor(fromNumber + Math.random() * (toNumber + 1 - fromNumber)); // Конструкцию взяла с https://learn.javascript.ru/task/random-int-min-max;
+    }
+    return Math.floor(fromNumber + Math.random() * (toNumber - fromNumber));
+  }
 }
-getSum(1, 2);
+rangeNumbers(1, 2);
+
+function randomNumber (fromNumber, toNumber,  signsAfterComma) {
+  if (fromNumber >= 0 && toNumber > fromNumber) {
+    let rangeNumber = fromNumber + Math.random() * (toNumber - fromNumber);
+
+    if (toNumber === parseInt(toNumber, 10)) {
+      rangeNumber = fromNumber + Math.random() * (toNumber + 1 - fromNumber);
+    }
+
+    return rangeNumber.toFixed(signsAfterComma);
+  }
+}
+randomNumber(1, 5, 10);
