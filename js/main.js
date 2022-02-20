a3ea91b62f4f8ae59a06da90b74403a7bffc0211
Irina Lukyanova <runnaf@mail.ru>
2022-02-20 15:35:41 +0300

    добавила обработчик ошибок

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 function getRandonInteger (fromNumber, toNumber) {
-  if (fromNumber >= 0 && toNumber > fromNumber) {
-    if (toNumber === parseInt(toNumber, 10)) {
+  if (fromNumber >= 0 && toNumber > fromNumber) { //проверяю диапазон заданных чисел положительные или нет
+    if (toNumber === parseInt(toNumber, 10)) { // проверяю целочисленное ли краевое значение диапазона
       return Math.floor(fromNumber + Math.random() * (toNumber + 1 - fromNumber)); // Конструкцию взяла с https://learn.javascript.ru/task/random-int-min-max;
-    }
+    } //если нет то в рандомное число не могу взять краевое значение включительно
     return Math.floor(fromNumber + Math.random() * (toNumber - fromNumber));
   }
+  alert('Вы ввели неправильное значение');
 }
 getRandonInteger(1, 2);
 
 function getRandomNumber (fromNumber, toNumber,  signsAfterComma) {
-  if (fromNumber >= 0 && toNumber > fromNumber) {
+  if (fromNumber >= 0 && toNumber > fromNumber) { // проверяю на целочисленное ли значение диапазона;
+    if (signsAfterComma === 0) { //если количество знаков после запятой 0, то toFixed не подходит, он округляет значение
+      return getRandonInteger(fromNumber, toNumber);
+    }
     return (fromNumber + Math.random() * (toNumber - fromNumber)).toFixed(signsAfterComma);
   }
+  alert('Вы ввели неправильное значение');
 }
-
 getRandomNumber(1, 5, 10);
