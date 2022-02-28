88191a6562e81d6a38ddc2851b8c40e8343aa937
Irina Lukyanova <runnaf@mail.ru>
2022-02-28 15:18:58 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,19 +1,116 @@
-function getRandonInteger (fromNumber, toNumber) {
-  if (fromNumber >= 0 && toNumber > fromNumber) { //проверяю диапазон заданных чисел положительные или нет
-    if (toNumber === parseInt(toNumber, 10)) { // проверяю целочисленное ли краевое значение диапазона
-      return Math.floor(fromNumber + Math.random() * (toNumber + 1 - fromNumber)); // Конструкцию взяла с https://learn.javascript.ru/task/random-int-min-max;
-    } //если нет то в рандомное число не могу взять краевое значение включительно
-    return Math.floor(fromNumber + Math.random() * (toNumber - fromNumber));
-  }
-}
-getRandonInteger(1, 2);
+function getRandomInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
 
-function getRandomNumber (fromNumber, toNumber,  signsAfterComma) {
-  if (fromNumber >= 0 && toNumber > fromNumber) { // проверяю на целочисленное ли значение диапазона;
-    if (signsAfterComma === 0) { //если количество знаков после запятой 0, то toFixed не подходит, он округляет значение
-      return getRandonInteger(fromNumber, toNumber);
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+function getRandomFloat (a, b,  digits = 1) {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+
+  const result = Math.random() * (upper - lower) + lower;
+  return +result.toFixed(digits);
+}
+
+function getRandomArrayElement(elements) {
+  return elements[getRandomInteger(0, elements.length - 1)];
+}
+
+function getRandomArray (sourceArray, numberOfArrayElements) {
+  const randomArray = [];
+  for (let i=0; i<numberOfArrayElements; i++) {
+    const elementRandomArray = getRandomArrayElement (sourceArray);
+    if (randomArray.indexOf(elementRandomArray) === -1)
+    {randomArray.push(elementRandomArray);}
+  }
+  return randomArray;
+}
+
+const SIMILAR_OBJECT_COUNT = 10;
+
+function getAuthorsArray() {
+  const authors = [];
+  for (let i=0; i<= SIMILAR_OBJECT_COUNT - 1; i++) {
+    let randomUserNumber = i + 1;
+
+    if (i < 9) {
+      randomUserNumber = '0'.concat(i + 1);
     }
-    return (fromNumber + Math.random() * (toNumber - fromNumber)).toFixed(signsAfterComma);
+
+    authors[i] = {
+      avatar : `img/avatars/user${  randomUserNumber  }.png`,
+    };
   }
 }
-getRandomNumber(1, 5, 10);
+getAuthorsArray();
+
+function getLocationArray() {
+  const locations = [];
+  for (let i=0; i<= SIMILAR_OBJECT_COUNT - 1; i++) {
+    const location = {
+      lat: getRandomFloat(35.65000, 35.70000, 5),
+      lng: getRandomFloat(139.70000, 139.80000, 5),
+    };
+    locations.push(location);
+  }
+  return locations;
+}
+
+const CHECKINS = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const CHECKOUTS = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const FEATURES_ARRAY = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const PHOTOS_ARRAY = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const MAX_PRICE = 100000;
+
+const MAX_ROOMS = 5;
+
+const MAX_GUESTS = 10;
+
+const offer = () => ({
+  title: 'Наше предложение по аренде Вам понравится',
+  address: getRandomArrayElement(getLocationArray()),
+  price: getRandomInteger(0, MAX_PRICE),
+  type: getRandomArrayElement(TYPES),
+  rooms: getRandomInteger(1, MAX_ROOMS),
+  guests: getRandomInteger(1, MAX_GUESTS),
+  checkin: getRandomArrayElement(CHECKINS),
+  checkout: getRandomArrayElement(CHECKOUTS),
+  features: getRandomArray(FEATURES_ARRAY, getRandomInteger (1, FEATURES_ARRAY.length)),
+  description: 'Просторное помещение с прекрасным видом',
+  photos: getRandomArray(PHOTOS_ARRAY, getRandomInteger (1, PHOTOS_ARRAY.length)),
+});
+
+const similarOffers = Array.from({length: SIMILAR_OBJECT_COUNT}, offer);
