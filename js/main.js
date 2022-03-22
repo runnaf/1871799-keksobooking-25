62c3b34018fb0c7d4043215bac12df3f0689f60d
Irina Lukyanova <runnaf@mail.ru>
2022-03-22 19:06:10 +0300

    доделала первую валидацию формы

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 import {getArrayElement} from './data.js';
 import {getSimilarElements} from './generating-similar-elements.js';
-import {activateForms, deactivateForms} from './state-of-form.js';
+// import {activateForm, deactivateForm} from './state-of-form.js';
 import './validator-form.js';
 
 const NUMBER_ADVERD_OFFER = 7;
 getSimilarElements(getArrayElement[NUMBER_ADVERD_OFFER]);
 
-activateForms();
-deactivateForms();
+// activateForm();
+// deactivateForm();
--- js/validator-form.js
+++ js/validator-form.js
@@ -3,24 +3,23 @@ const MIN_VALUE_TITLE = 30;
 const MAX_VALUE_TITLE = 100;
 const MAX_VALUE_PRICE = 100000;
 const pristine = new Pristine(form, {
-  classTo: 'ad-form__label',
-  errorClass: 'ad-form__label--invalid',
-  successClass: 'ad-form__label--valid',
-  errorTextParent: 'ad-form__label',
-  errorTextTag: 'p',
-  errorTextClass: 'error',
+  classTo: 'ad-form__element',
+  errorTextParent: 'ad-form__element',
+  errorTextClass: 'ad-form__error-text',
+  errorTextTag: 'div',
 });
 
+const titleField = form.querySelector('#title');
 
 function validateTitle (value) {
   return value.length >= MIN_VALUE_TITLE && value.length <= MAX_VALUE_TITLE;
 }
 
-pristine.addValidator(
-  form.querySelector('#title'),
-  validateTitle,
-  'От 30 до 100 символов'
-);
+function getTitleErrorMessage () {
+  return 'от 30 до 100 символов';
+}
+
+pristine.addValidator(titleField, validateTitle, getTitleErrorMessage);
 
 const priceField = form.querySelector('#price');
 const minPrice = {
@@ -30,20 +29,17 @@ const minPrice = {
   'house': 5000,
   'palace': 10000,
 };
-const typeContainer = form.querySelector('[name="type"]');
 
 function validatePrice (price) {
-  const typeOfHous = Array.from(typeContainer.children.selected);
-  const minPriceOfArray = typeOfHous.sort().shift();
-  return price <= MAX_VALUE_PRICE && price >= minPrice[minPriceOfArray.value];
+  const typeOfHouseField = form.querySelector('[name="type"]');
+  return price <= MAX_VALUE_PRICE && price >= minPrice[typeOfHouseField.value];
 }
 
 function getPriceErrorMessage (price) {
   if (price < MAX_VALUE_PRICE) {
-    const typeOfHous = Array.from(typeContainer.children.selected);
-    const minPriceOfArray = typeOfHous.sort().shift();
+    const typeOfHouseField = form.querySelector('[name="type"]');
 
-    return `Минимальная цена за ночь должна быть больше ${minPrice[minPriceOfArray.value]}`;
+    return `Минимальная цена за ночь должна быть больше ${minPrice[typeOfHouseField.value]}`;
   } else {
     return `Минимальная цена за ночь должна быть меньше ${MAX_VALUE_PRICE}`;
   }
@@ -51,30 +47,32 @@ function getPriceErrorMessage (price) {
 
 pristine.addValidator(priceField, validatePrice, getPriceErrorMessage);
 
-function ontypeOfHousChange () {
+function onTypeOfHousChange () {
   priceField.placeholder = minPrice[this.value];
   pristine.validate(priceField);
 }
 
-typeContainer.children.foreach((item) => item.addEventListener('change', ontypeOfHousChange));
+form.querySelectorAll('[name="type"]').forEach((item) => item.addEventListener('change', onTypeOfHousChange));
 
 
 const numberOfRoomsField = form.querySelector('[name="rooms"]');
-const numberOfCapacityField = form.querySelector('[capacity]');
+const numberOfCapacityField = form.querySelector('[name="capacity"]');
 const roomsOption = {
-  '1 комната': ['для 1 гостя'],
-  '2 комнаты': ['для 2 гостей', 'для 1 гостя'],
-  '3 комнаты': ['для 3 гостей', 'для 2 гостей', 'для 1 гостя'],
-  '100 комнат': ['не для гостей'],
+  '1': ['1'],
+  '2': ['2', '1'],
+  '3': ['3', '2', '1'],
+  '100': ['0'],
 };
 
 function validateRooms () {
   return roomsOption[numberOfRoomsField.value].includes(numberOfCapacityField.value);
 }
 
-const getRoomsErrorMessage = () =>(numberOfRoomsField.value === '100' || numberOfCapacityField.value === '0')
-  ? '100 комнат не для гостей'
-  : 'количество комнат не должно превышать количество гостей';
+function getRoomsErrorMessage () {
+  if (numberOfRoomsField.value === '100' || numberOfCapacityField.value === '0') {
+    return '100 комнат не для гостей';
+  } return 'количество комнат не должно превышать количество гостей';
+}
 
 pristine.addValidator(numberOfRoomsField, validateRooms, getRoomsErrorMessage);
 pristine.addValidator(numberOfCapacityField, validateRooms, getRoomsErrorMessage);
