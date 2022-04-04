64e205714c58636bf7af8f26209588a42c12f0f6
Irina Lukyanova <runnaf@mail.ru>
2022-04-04 21:29:53 +0300

    Перламутровые пуговицы

--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
 import './validator-form.js';
 import './map.js';
 import './get-slider-element.js';
-import {renderSimilarPopap} from './map.js';
+import {renderSimilarPopap, setTypeClick, setRoomsClick, setGuestsClick, setFeaturesClick, setPriceClick} from './map.js';
 import { setUserFormSubmit } from './submiting-form.js';
 import './reset-form.js';
 import {getErrorMessage} from './get-message.js';
-
-const SIMILAR_ADS = 10;
 
 
 fetch('https://25.javascript.pages.academy/keksobooking/data')
   .then((response) => response.json())
   .then((offerElements) => {
-    renderSimilarPopap(offerElements.slice(0, SIMILAR_ADS));
+    renderSimilarPopap(offerElements);
+    setTypeClick(() => {
+      renderSimilarPopap(offerElements);
+    });
+    setRoomsClick(() => {
+      renderSimilarPopap(offerElements);
+    });
+    setGuestsClick(() => {
+      renderSimilarPopap(offerElements);
+    });
+    setFeaturesClick(() => {
+      renderSimilarPopap(offerElements);
+    });
+    setPriceClick(() => {
+      renderSimilarPopap(offerElements);
+    });
   })
   .catch(() => getErrorMessage()
   );
