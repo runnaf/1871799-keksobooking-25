ee9eff27fddb3bb788af10b8d050b1f5a18692e2
Irina Lukyanova <runnaf@mail.ru>
2022-04-05 23:02:01 +0300

    внесла правки по замечаниям, но убрала отдельную функцию ля фото

--- js/avatar.js
+++ js/avatar.js
@@ -1,6 +1,14 @@
-import { addPhoto } from './util.js';
-
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 const fileChooser = document.querySelector('.ad-form__input-picture--avatar');
 const preview = document.querySelector('.ad-form-header__img');
 
-fileChooser.addEventListener('change', addPhoto(fileChooser, preview));
+fileChooser.addEventListener('change', () => {
+  const file = fileChooser.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+
+  if (matches) {
+    preview.src = URL.createObjectURL(file);
+  }
+});
--- js/util.js
+++ js/util.js
@@ -1,32 +1,4 @@
 const RERENDER_DELAY = 500;
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-const getRandomFloat = (a, b,  digits = 1) => {
-  const lower = Math.min(Math.abs(a), Math.abs(b));
-  const upper = Math.max(Math.abs(a), Math.abs(b));
-
-  const result = Math.random() * (upper - lower) + lower;
-  return +result.toFixed(digits);
-};
-
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-
-const getRandomArray = (sourceArray, numberOfArrayElements) => {
-  const randomArray = [];
-  for (let i=0; i<numberOfArrayElements; i++) {
-    const elementRandomArray = getRandomArrayElement (sourceArray);
-    if (randomArray.indexOf(elementRandomArray) === -1)
-    {randomArray.push(elementRandomArray);}
-  }
-  return randomArray;
-};
-
 const addClass = (element, classToAdd) => {
   const className = element.classList.add(classToAdd);
   return className;
@@ -59,4 +31,4 @@ const addPhoto = (Input, elementPhoto) => {
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
-export {getRandomInteger, getRandomFloat, getRandomArrayElement, getRandomArray, addClass, removeClass, isEscapeKey, debounce, addPhoto};
+export {addClass, removeClass, isEscapeKey, debounce, addPhoto};
