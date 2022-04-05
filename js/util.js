1c9877171b54f4c60e1b473a351b98ce50b40c14
Irina Lukyanova <runnaf@mail.ru>
2022-04-05 23:04:30 +0300

    убрала функцию addPhoto из util

--- js/util.js
+++ js/util.js
@@ -16,19 +16,7 @@ const debounce = (callback, timeoutDelay = RERENDER_DELAY) => {
     timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
   };
 };
-const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
-
-const addPhoto = (Input, elementPhoto) => {
-  const file = Input.files[0];
-  const fileName = file.name.toLowerCase();
-
-  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
-
-  if (matches) {
-    elementPhoto.src = URL.createObjectURL(file);
-  }
-};
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
-export {addClass, removeClass, isEscapeKey, debounce, addPhoto};
+export {addClass, removeClass, isEscapeKey, debounce};
