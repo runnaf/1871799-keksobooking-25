c31570b13d62b5e9a535189502e0f2cbf6356df3
Irina Lukyanova <runnaf@mail.ru>
2022-04-08 09:36:07 +0300

    исправила ф-цию валидации обратно

--- js/validator-form.js
+++ js/validator-form.js
@@ -59,11 +59,11 @@ const validatorForm = () => {
 
   pristine.addValidator(titleFieldElement, validateTitle, getTitleErrorMessage);
   pristine.addValidator(priceFieldElement, validatePrice, getPriceErrorMessage);
-  const onTypeOfHousChange = () => {
+  formElement.querySelectorAll('[name="type"]').forEach((item) => item.addEventListener('change', onTypeOfHousChange));
+  function onTypeOfHousChange () {
     priceFieldElement.placeholder = minPrice[this.value];
     pristine.validate(priceFieldElement);
-  };
-  formElement.querySelectorAll('[name="type"]').forEach((item) => item.addEventListener('change', onTypeOfHousChange));
+  }
   pristine.addValidator(numberOfRoomsField, validateRooms, getRoomsErrorMessage);
   pristine.addValidator(numberOfCapacityField, validateRooms, getRoomsErrorMessage);
   const isValid = pristine.validate();
